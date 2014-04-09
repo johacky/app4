302bc4fe9afad351b45042f5023eb86a74be3941
johacky <johack@mail.ru>
2014-04-09 20:49:04 +0300

    11

--- js/index.js
+++ js/index.js
@@ -44,6 +44,8 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
+		window.location.href = "http://ewba.co.il";
+		
         console.log('Received Event: ' + id);
     }
 };
